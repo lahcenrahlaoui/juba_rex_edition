ab800572d88bd4a5df37f50da9e5235d88d46f40
lahcenrahlaoui <lahcen.rahlaoui@univ-tiaret.dz>
2024-10-16 19:55:45 +0100

    add new files

--- src/app/artefacts/[id]/page.jsx
+++ src/app/artefacts/[id]/page.jsx
@@ -4,24 +4,72 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 import ModelViewer from "@/components/ModelViewer";
 import { items } from "@/utils/db.js";
+import { motion } from "framer-motion"; // Import motion
+import { useUser } from "@/context/userContext";
+import { useRouter } from "next/navigation";
 
 const ItemDetail = ({ params }) => {
     const { id } = params;
+    const [isLoading, setIsLoading] = useState(true);
     const [showItem, setShowItem] = useState({});
 
+    const { login } = useUser();
+
     useEffect(() => {
-        const selectedItem = items.find((item) => item.id === id);
-        console.log(selectedItem)
-        setShowItem(selectedItem || {});
+        const userExists = localStorage.getItem("user") !== null;
+        if (userExists) {
+            login();
+        }
+
+        console.log("showItem");
+    }, []);
+
+    const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsLoading(true);
+            const selectedItem = items.find((item) => item.id === id);
+            console.log(selectedItem);
+            setShowItem(selectedItem || {});
+            (async () => {
+                await delay(2000);
+                setIsLoading(false);
+            })();
+        };
+
+        fetchData();
     }, [id]);
 
+    if (isLoading) {
+        return (
+            <motion.div
+                className="flex items-center justify-center h-screen"
+                initial={{ opacity: 1 }} // Start fully visible
+                animate={{ opacity: 0 }} // Fade out
+                transition={{ duration: 2 }} // Duration of fade-out effect
+            >
+                <div className="absolute inset-0 flex items-center justify-center   ">
+                    <div className="relative flex flex-col items-center">
+                        <div className="w-24 h-24 rounded-full border-4 border-transparent border-t-[#FFB800] animate-spin" />
+                        <div className="w-20 h-20 rounded-full bg-[#FFB800] animate-pulse absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+                    </div>
+                </div>
+            </motion.div>
+        );
+    }
+
     return (
-        <div className="relative container mx-auto h-screen p-4 sm:p-8 bg-white dark:bg-black rounded-lg transition-colors duration-300 ease-in-out mb-20"> {/* Color transition */}
+        <motion.div
+            className="relative container mx-auto h-screen p-4 sm:p-8 bg-white dark:bg-black rounded-lg transition-colors duration-300 ease-in-out mb-20"
+            initial={{ opacity: 0 }} // Start fully transparent
+            animate={{ opacity: 1 }} // Fade in
+            transition={{ duration: 0.5 }} // Duration of fade-in effect
+        >
             {/* Title */}
             <h1 className="text-3xl sm:text-4xl font-bold text-black dark:text-[#FFB800] mb-4 sm:mb-6 text-center">
                 {showItem?.name}
             </h1>
-
             {/* Back button */}
             <div className="absolute top-4 sm:top-8 left-4">
                 <Link href="/artefacts" passHref>
@@ -40,37 +88,29 @@ const ItemDetail = ({ params }) => {
                                 d="M15 12H3m0 0l9-9m-9 9l9 9"
                             />
                         </svg>
-                        <span className="ml-2 text-black dark:text-[#FFB800] transition-colors duration-300 ease-in-out">Back</span>
+                        <span className="ml-2 text-black dark:text-[#FFB800] transition-colors duration-300 ease-in-out">
+                            Back
+                        </span>
                     </button>
                 </Link>
             </div>
+            {/* Content Section */}
+            <div className="flex flex-col md:flex-row items-center justify-center mt-0 h-auto md:h-[80%] rounded-lg overflow-hidden">
+                {/* Description - 2/5 width on larger screens */}
+                <div className="flex-1 md:flex-[2] flex flex-col items-center justify-center mb-4 sm:mb-8 px-4 sm:px-6 order-2 md:order-1">
+                    <p className="text-gray-900 dark:text-white text-base sm:text-lg max-w-xs sm:max-w-md p-4 sm:p-6 bg-gradient-to-r from-gray-100 to-gray-200 dark:from-gray-800 dark:to-gray-900 rounded-lg shadow-lg leading-relaxed text-center">
+                        {showItem?.description}
+                    </p>
+                </div>
 
-           {/* Content Section */}
-           <div className="flex flex-col md:flex-row items-center justify-center mt-0 h-auto md:h-[80%] rounded-lg overflow-hidden">
-    {/* Description - 2/5 width on larger screens */}
-    <div className="flex-1 md:flex-[2] flex flex-col items-center justify-center mb-4 sm:mb-8 px-4 sm:px-6 order-2 md:order-1"> {/* Added md:order-2 for reverse order on larger screens */}
-        <p className="text-gray-900 dark:text-white text-base sm:text-lg max-w-xs sm:max-w-md p-4 sm:p-6 bg-gradient-to-r from-gray-100 to-gray-200 dark:from-gray-800 dark:to-gray-900 rounded-lg shadow-lg leading-relaxed text-center">
-            {showItem?.description}
-        </p>
-    </div>
-
-    {/* Model Viewer - 3/5 width on larger screens */}
-    <div className="w-96 flex-1 md:flex-[3] flex items-center justify-center px-4 sm:px-6 order-1 md:order-2"> {/* Added md:order-1 for normal order on larger screens */}
-        <div className="w-full h-full flex justify-center items-center">
-            <ModelViewer
-                fileUrl={showItem?.fileUrl}
-                className="w-full h-full rounded-lg shadow-lg" // Adjusted shadow for consistency
-            />
-        </div>
-    </div>
-</div>
-
-
-
-        
-        
-
-        </div>
+                {/* Model Viewer - 3/5 width on larger screens */}
+                <div className="w-[25rem] h-[25rem] flex-1 md:flex-[3] flex items-center justify-center px-4 sm:px-6 order-1 md:order-2">
+                    <div className="w-full h-full flex justify-center items-center">
+                        <ModelViewer fileUrl={showItem?.fileUrl} />
+                    </div>
+                </div>
+            </div>
+        </motion.div>
     );
 };
 
--- src/components/ModelViewer.jsx
+++ src/components/ModelViewer.jsx
@@ -1,46 +1,158 @@
 "use client";
 
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import "@google/model-viewer";
+import { motion } from "framer-motion"; // Import motion
 
 export default function ModelViewer({ fileUrl }) {
-  const modelRef = useRef(null);
+    const modelRef = useRef(null);
+    const [modelLoaded, setModelLoaded] = useState(false);
 
-  useEffect(() => {
-    const modelViewer = modelRef.current;
-    console.log({modelViewer})
+    const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-    // Example: Add a listener for when the model is loaded
-    modelViewer?.addEventListener('load', () => {
-      console.log('Model loaded!');
+    useEffect(() => {
+        console.log("file url");
+        console.log(fileUrl);
+        console.log("file url");
+        const modelViewer = modelRef.current;
 
-      // Set the initial scale of the model
-      modelViewer.setAttribute('scale', '1.4 1.4 1.4'); // Adjust the scale as needed
+        const handleModelLoad = () => {
+            console.log("Model loaded successfully!");
+            (async () => {
+                await delay(2000);
 
-      // Adjust the camera position
-      modelViewer.setAttribute('camera-orbit', '0deg 90deg 50m'); // Adjust the camera angle and distance
-    });
+                setModelLoaded(true);
+            })();
+            initiateAnimations(modelViewer);
+        };
 
-    return () => {
-      modelViewer?.removeEventListener('load', () => {
-        console.log('Model loaded!');
-      });
+        const handleModelError = (error) => {
+            console.error("Error loading model:", error);
+            console.error("Error details:", error.detail);
+        };
+
+        modelViewer?.addEventListener("load", handleModelLoad);
+        modelViewer?.addEventListener("error", handleModelError);
+
+        return () => {
+            modelViewer?.removeEventListener("load", handleModelLoad);
+            modelViewer?.removeEventListener("error", handleModelError);
+        };
+    }, [fileUrl]);
+
+    const initiateAnimations = (modelViewer) => {
+        const duration = 2000;
+        const steps = 100;
+
+        // Scale Animation
+        animateScale(modelViewer, 1, duration, steps);
+
+        // Camera Orbit Animation
+        const { initialOrbit, targetOrbit } = getOrbitSettings(fileUrl);
+        animateCameraOrbit(
+            modelViewer,
+            initialOrbit,
+            targetOrbit,
+            duration,
+            steps
+        );
     };
-  }, []);
 
-  return (
-    <div className="w-full h-96 flex items-center justify-center">
-      {fileUrl}
-      <model-viewer
-        ref={modelRef}
-        src={fileUrl}
-        alt="A 3D model"
-        auto-rotate
-        camera-controls
-        style={{ width: '100%', height: '100%' }}
-       
-      >
-      </model-viewer>
-    </div>
-  );
+    const getOrbitSettings = (fileUrl) => {
+        const isCoinModel = fileUrl === "/assets/3d/JUBA_COIN.glb";
+        return {
+            initialOrbit: isCoinModel ? "0deg 0deg 50m" : "0deg 90deg 50m",
+            targetOrbit: isCoinModel ? "90deg 0deg 50m" : "0deg 90deg 50m",
+        };
+    };
+
+    const animateScale = (modelViewer, targetScale, duration, steps) => {
+        let scaleValue = 0;
+        const increment = targetScale / steps;
+        const interval = duration / steps;
+
+        const easeInOut = (t) => (t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t);
+
+        const scaleInterval = setInterval(() => {
+            scaleValue = Math.min(scaleValue + increment, targetScale);
+            const easedScale =
+                easeInOut(scaleValue / targetScale) * targetScale;
+            modelViewer.setAttribute(
+                "scale",
+                `${easedScale} ${easedScale} ${easedScale}`
+            );
+            if (scaleValue >= targetScale) clearInterval(scaleInterval);
+        }, interval);
+    };
+
+    const animateCameraOrbit = (
+        modelViewer,
+        initialOrbit,
+        targetOrbit,
+        duration,
+        steps
+    ) => {
+        const [initialAzimuth, initialElevation] = initialOrbit
+            .split(" ")
+            .map(parseFloat);
+        const [targetAzimuth, targetElevation] = targetOrbit
+            .split(" ")
+            .map(parseFloat);
+
+        let currentAzimuth = initialAzimuth;
+        let currentElevation = initialElevation;
+        const azimuthIncrement = (targetAzimuth - initialAzimuth) / steps;
+        const elevationIncrement = (targetElevation - initialElevation) / steps;
+        const interval = duration / steps;
+
+        const orbitInterval = setInterval(() => {
+            currentAzimuth += azimuthIncrement;
+            currentElevation += elevationIncrement;
+
+            modelViewer.setAttribute(
+                "camera-orbit",
+                `${currentAzimuth}deg ${currentElevation}deg 50m`
+            );
+            if (
+                Math.abs(currentAzimuth - targetAzimuth) < 0.01 &&
+                Math.abs(currentElevation - targetElevation) < 0.01
+            ) {
+                clearInterval(orbitInterval);
+            }
+        }, interval);
+    };
+
+    return (
+        <div className="w-full h-full flex items-center justify-center relative">
+            {!modelLoaded && (
+                <motion.div
+                    className="flex items-center justify-center h-screen"
+                    initial={{ opacity: 1 }} // Start fully visible
+                    animate={{ opacity: 0 }} // Fade out
+                    transition={{ duration: 4 }} // Duration of fade-out effect
+                >
+                    <div className="absolute inset-0 flex items-center justify-center   ">
+                        <div className="relative flex flex-col items-center">
+                            <div className="w-24 h-24 rounded-full border-4 border-transparent border-t-[#FFB800] animate-spin" />
+                            <div className="w-20 h-20 rounded-full bg-[#FFB800] animate-pulse absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+                        </div>
+                    </div>
+                </motion.div>
+            )}
+
+            <model-viewer
+                ref={modelRef}
+                src={fileUrl}
+                alt="A 3D model"
+                auto-rotate
+                camera-controls
+                style={{
+                    width: "100%",
+                    height: "100%",
+                    scale: modelLoaded ? "1" : "0",
+                    transition: "scale 2s ease-in-out", // Smooth scaling
+                }}
+            />
+        </div>
+    );
 }
