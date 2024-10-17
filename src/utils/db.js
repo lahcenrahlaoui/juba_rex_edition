8cf12b663e555385c7473956baa587a5a3cb9b24
lahcenrahlaoui <lahcen.rahlaoui@univ-tiaret.dz>
2024-10-17 08:50:30 +0100

    change db language

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,44 +1,52 @@
 export const items = [
-  { 
-      id: "1", 
-      name: "Bijoux Femme Touareg", 
-      imageUrl: "/assets/images/Bijoux_Touareg.png", 
-      fileUrl: "/assets/3d/Bijoux_Femme_Touareg_Platform.glb", 
-      description: "The Bijoux Femme Touareg features intricate designs that symbolize status and identity within the Tuareg community, reflecting the rich cultural heritage of its artisans." 
+  {
+      id: "1",
+      name: "JUBA Fibule Logo",
+      imageUrl: "/assets/images/Logo.png",
+      fileUrl: "/assets/3d/JUBA_FIBULE_LOGO.glb",
+      description:
+          "Cette fibule décorative sert à la fois à des fins fonctionnelles et esthétiques, incarnant l'artisanat régional et le patrimoine tout en ajoutant de l'élégance aux vêtements et en reflétant l'identité culturelle.",
   },
-  { 
-      id: "2", 
-      name: "El Mrach Ethnographique", 
-      imageUrl: "/assets/images/El_MRACH.jpg", 
-      fileUrl: "/assets/3d/El_Mrach_ethnographique.glb", 
-      description: "El Mrach is an exquisite object used for sprinkling rose water during various cultural rituals, enhancing hospitality and creating a welcoming atmosphere for honored guests." 
+  {
+      id: "2",
+      name: "JUBA Fibule Smaller",
+      imageUrl: "/assets/images/Fibule_Turntable.png",
+      fileUrl: "/assets/3d/JUBA_FIBULE_Smaller.glb",
+      description:
+          "La petite fibule JUBA est une version raffinée de la fibule traditionnelle, présentant élégance et minutie, et représentant le savoir-faire des artisans de la région.",
   },
-  { 
-      id: "3", 
-      name: "IMA Desert Smaller", 
-      imageUrl: "/assets/images/IMA_Arabo_Futur.png", 
-      fileUrl: "/assets/3d/IMA_Desert_Smaller.glb", 
-      description: "This model showcases the stunning beauty of the IMA Desert's vast landscapes and geological formations, highlighting the unique ecology and diversity of its flora and fauna." 
+  {
+      id: "3",
+
+      name: "JUBA Coin",
+      imageUrl: "/assets/images/Coin_Turntable1.png",
+      fileUrl: "/assets/3d/JUBA_COIN.glb",
+      description:
+          "La pièce JUBA reflète la riche histoire des pratiques commerciales anciennes et des structures économiques, servant de lien tangible avec le passé et illustrant la signification culturelle.",
   },
-  { 
-      id: "4", 
-      name: "JUBA Coin", 
-      imageUrl: "/assets/images/Coin_Turntable1.png", 
-      fileUrl: "/assets/3d/JUBA_COIN.glb", 
-      description: "The JUBA Coin reflects the rich history of ancient trade practices and economic structures, serving as a tangible link to the past and illustrating cultural significance." 
+  {
+      id: "4",
+
+      name: "IMA Institut de Mode Arabe",
+      imageUrl: "/assets/images/IMA_Arabo_Futur.png",
+      fileUrl: "/assets/3d/IMA_Desert_Smaller.glb",
+      description:
+          "Ce modèle met en valeur la beauté époustouflante des vastes paysages du désert de l'IMA et ses formations géologiques, soulignant l'écologie unique et la diversité de sa flore et de sa faune.",
   },
-  { 
-      id: "5", 
-      name: "JUBA Fibule Logo", 
-      imageUrl: "/assets/images/Logo.png", 
-      fileUrl: "/assets/3d/JUBA_FIBULE_LOGO.glb", 
-      description: "This decorative fibula serves both functional and aesthetic purposes, embodying regional craftsmanship and heritage while adding elegance to garments and reflecting cultural identity." 
+  {
+      id: "5",
+      name: "El Mrach Ethnographique",
+      imageUrl: "/assets/images/El_MRACH.jpg",
+      fileUrl: "/assets/3d/El_Mrach_ethnographique.glb",
+      description:
+          "El Mrach est un objet exquis utilisé pour asperger de l'eau de rose lors de divers rituels culturels, améliorant l'hospitalité et créant une atmosphère accueillante pour les invités d'honneur.",
   },
-  { 
-      id: "6", 
-      name: "JUBA Fibule Smaller", 
-      imageUrl: "/assets/images/Fibule_Turntable.png", 
-      fileUrl: "/assets/3d/JUBA_FIBULE_Smaller.glb", 
-      description: "The JUBA Fibule Smaller is a refined version of the traditional fibula, showcasing elegance and intricacy, and representing the skill of artisans from the region." 
-  }
+  {
+      id: "6",
+      name: "Bijoux Femme Touareg",
+      imageUrl: "/assets/images/Bijoux_Touareg.png",
+      fileUrl: "/assets/3d/Bijoux_Femme_Touareg_Platform.glb",
+      description:
+          "Le Bijoux Femme Touareg présente des motifs complexes symbolisant le statut et l'identité au sein de la communauté touarègue, reflétant le riche patrimoine culturel de ses artisans.",
+  },
 ];
